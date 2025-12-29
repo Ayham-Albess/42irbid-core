89a346acbb1e55cdc45d879aa3e7b7ccedfa77e0
Ayham-Albess <aihamalbess12@gmail.com>
2025-12-29 10:15:15 +0300

    adding get next line

--- getnextline/get_next_line.c
+++ getnextline/get_next_line.c
@@ -1,4 +1,3 @@
-
 #include "get_next_line.h"
 
 char	*ft_join_and_free(char *text, char *buffer)
--- getnextline/get_next_line.h
+++ getnextline/get_next_line.h
@@ -1,8 +1,8 @@
-#ifndef  GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define  BUFFER_SIZE 1234
+#  define BUFFER_SIZE 1234
 # endif
 
 # include <unistd.h>
