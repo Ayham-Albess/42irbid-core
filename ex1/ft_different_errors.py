18376d09386647ae2ab8a6aeb777bf7317db300d
Ayham-Albess <aihamalbess12@gmail.com>
2026-03-09 17:24:32 +0300

    Add files via upload

---
+++ ex1/ft_different_errors.py
@@ -0,0 +1,40 @@
+def garden_operations():
+    try:
+        s = "Caught ValueError: invalid literal for int()\n"
+        print("Testing ValueError...")
+        x = "abc"
+        for i in x:
+            if (i < '0' or i > '9'):
+                raise ValueError(s)
+    except ValueError as e:
+        print(e)
+    try:
+        print("Testing ZeroDivisionError...")
+        x = 15/0
+        x = x*x
+    except ZeroDivisionError:
+        print("Caught ZeroDivisionError: division by zero\n")
+    try:
+        print("Testing FileNotFoundError...")
+        f = open("texrt.txt")
+        f = f - "a"
+    except FileNotFoundError:
+        print("Caught FileNotFoundError: No such file ’texrt.txt’\n")
+    try:
+        print("Testing KeyError...")
+        direc = {'elbess': 'ayham'}
+        s = direc['rosan']
+    except KeyError:
+        print("Caught KeyError: ’elbess’\n")
+    try:
+        print("Testing multiple errors together...")
+        x = "abc"/0
+        x = x*x
+    except (TypeError, ZeroDivisionError):
+        print("Caught an error, but program continues!\n")
+    print("All error types tested successfully!")
+
+
+if __name__ == "__main__":
+    print("=== Garden Error Types Demo ===\n")
+    garden_operations()
